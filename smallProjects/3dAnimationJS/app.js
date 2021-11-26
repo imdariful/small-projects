8667f34c6021da6195d67083b32dda0a1da8c0ac
Md. Ariful Islam <ariful.rony10@gmail.com>
2021-11-26 12:11:46 +0600

    3d animation created
    3d animation created

---
+++ smallProjects/3dAnimationJS/app.js
@@ -0,0 +1,17 @@
+// Movement Animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+// Moving animation Event
+container.addEventListener('mousemove', (e) =>{
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// Animation In
+
+// Animation Out
+container.addEventListener('mouseleave', (e) =>{
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+})
+
